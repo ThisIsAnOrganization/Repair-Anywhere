4f86d688ea6e6ebaee12c62b9d4e7cb6145523f3
macefti276 <macefti276@gmail.com>
2018-10-01 11:54:49 +0600

    Common

---
+++ RepairAnywhere/Controllers/CommonController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepairAnywhere.Controllers
+{
+    public class CommonController : Controller
+    {
+
+        public CommonController()
+        {
+
+        }
+
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        public ActionResult forgetpass()
+        {
+            return View();
+        }
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+
+
+    }
+}
